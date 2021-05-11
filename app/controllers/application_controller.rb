fcec02360a5225764f012d1f9beb36de2a7dc2ec
manabu-creater <69703387+manabu-creater@users.noreply.github.com>
2021-05-12 07:36:32 +0900

    devise導入＆top画面設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:company_name, :company_tel, :zip_code, :preferences_id, :address])
+  end
 end
