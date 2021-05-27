0e1410739996fe5e3c5aae3b20bd83b3a959940e
manabu-creater <69703387+manabu-creater@users.noreply.github.com>
2021-05-27 20:21:10 +0900

    カテゴリー別一覧表示の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :price, :amount, :detail, :locality, :expiry_date, :preservation, :category_id)
