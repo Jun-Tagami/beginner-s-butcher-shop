f782af8cb4b9bcd9760d3204db75313f85931f7c
Jun <zxc4627@gmail.com>
2021-06-01 21:06:08 +0900

    モデル作成_3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2021_05_21_115338) do
+=======
+ActiveRecord::Schema.define(version: 2021_05_24_222656) do
+>>>>>>> Stashed changes
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
