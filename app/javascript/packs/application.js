209928054ee9a21a637f2a92d5d041feeb93e2c2
manabu-creater <69703387+manabu-creater@users.noreply.github.com>
2021-06-09 21:42:22 +0900

    footer/JavaScript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,9 @@ require("../item_price");
 require("../navigation");
 require("../navigation2");
 require("../navigation3");
-
+require("../navigation4");
+require("../navigation5");
+require("../navigation6");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
