908a861efd8983b4aaab87078386634a3b0c0af4
manabu-creater <69703387+manabu-creater@users.noreply.github.com>
2021-07-04 20:17:29 +0900

    出品商品のテストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name     {'居酒屋 池袋'}
+    amount   {'1kg'}
+    price    {5000}
+    detail   {'美味しい'}
+    locality {'山梨県産'}
+    expiry_date {'2日間'}
+    preservation {'冷蔵10°以下'}
+    category_id  { 2 }
   end
 end
