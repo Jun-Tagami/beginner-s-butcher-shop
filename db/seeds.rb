468331b6344f878dbaf2a515cfcb66da7d90286d
Jun <zxc4627@gmail.com>
2021-07-18 15:35:11 +0900

    seed設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+users.create!(
+company-name:'test'
+company-tel:'000000000'
+email:'111@111'
+zip-code:'111-111'
+preferences-id:'2'
+address:'test'
+encrypted-password:'111111a'
+)
+
+
+items.create!(
+image: File.open('./app/assets/images/beef2.jpg')
+name:'test'
+amount:'2'
+price:'2'
+detail:'test'
+locality:'test'
+expiry-date:'test'
+preservataion:'test'
+)
