a4d2f46766f561f3832f29412c300c2c660f4764
Jun <zxc4627@gmail.com>
2021-07-25 20:54:23 +0900

    購入機能_11

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,33 +1,40 @@
-class OrdersController < ApplicationController
+class OrderController < ApplicationController
+
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :ordersopen, only: [:new, :create]
 
   # 復習用→ログアウト状態のユーザーは、URLを直接入力して商品購入ページに遷移しようとすると、商品の販売状況に関わらずログインページに遷移すること
   def new
     # 復習用→フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
-     @order = Order.new
-     @item = Item.find(params[:item_id])
+     @order_address = OrderAddress.new
   end
- 
+
   def create
-    #フロントから送信されたデータを受け取って、新規のorderオブジェクトを作成してインスタンス変数に代入する
-      @order= Order.new(order_params)
-      # @item = Item.find(params[:orders][:item_id])
-    #オーダーオフジェクトを保存する。保存ができたらトップページにリダイレクトする。
-    if @order.save
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
       redirect_to root_path
-    #オーダーオブジェクトの保存が失敗したら注文画面に戻る。 
     else
-      render :new
+      render action: :index
     end
   end
+
+  def show
+
 
 private
 
 def order_params
-  params.require(:orders).permit(:postal_code, :area_id, :municipalities, :house_number, :building_name, :phone_number).merge(
+  params.require(:order_address).permit(:postal_code, :area_id, :municipalities, :house_number, :building_name, :phone_number).merge(
     user_id: current_user.id, item_id: params[:item_id], token: params[:token]
   )
-end 
+end
+
+def ordersopen
+  @item = Item.find(params[:item_id])
+  # redirect_to root_path if @item.user.id == current_user.id || !@item.order.nil?
+end
 
 def pay_item
   Payjp.api_key = ENV['PAYJP_SECRET_KEY']
