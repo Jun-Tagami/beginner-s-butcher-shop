76fd54ed391ecb9085b0d07cf6d8dfdafa9cf690
Jun <zxc4627@gmail.com>
2021-09-16 14:10:58 +0900

    予約一覧_7

--- app/controllers/owner_reserve_controller.rb
+++ app/controllers/owner_reserve_controller.rb
@@ -1,8 +1,9 @@
 class OwnerReserveController < ApplicationController
   def index
-    @users = User.all
+    @user = User.all
     @reserves = Reserve.all
-    @orders = Order.all
+    @order = Order.all
+  
 
     # @order = current_user.orders.build(order_params)
     # @item = Item.find(params[:order][:item_id])
